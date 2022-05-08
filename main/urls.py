c194d65fff0779f6b77fbfb801d68ff3dab3a91a
IbrahimElmourchidi <ibrahimElmourchidi2@gmail.com>
2022-05-08 16:44:11 +0200

    story 002 (#3)

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from .views import *
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', home),
     path('category-list', category_list, name='category-list'),
+    path('brand-list', brand_list, name='brand-list'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
